94fb5bac7c57bdcec0f34752ff422c7607627b3c
Hankamongus <hbarterburn@gmail.com>
2024-03-12 10:59:11 -0500

    Progress oh progress move on if you must
    Just leave me that small patch of tall weeds and rust

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 
 <template>
     <!--Navbar---->
-    <nav id="navbar" class="w-full left-0 right-0 mt-0 bg-gradient-to-r from-gray-500 via-sky-700 to-gray-700 fixed left-0 w-full z-30 top-0 mb-5 p-1 text-lg transition-transform duration-300 transform">
+    <nav id="navbar" class="w-full left-0 right-0 mt-0 bg-gradient-to-r from-gray-500 via-sky-700 to-gray-500 fixed left-0 w-full z-30 top-0 mb-5 p-1 text-lg transition-transform duration-300 transform">
         <div class="text-black"><strong>Solid Rock Baptist</strong></div>
         <div class="mx-auto max-w-8xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
           <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
 
             <!-- Mobile menu button-->
-            <button @click="navOpen = !navOpen" type="button" class=" md:hidden relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+            <button @click="navOpen = !navOpen" type="button" class=" md:hidden relative inline-flex items-center justify-center left-0 rounded-md p-2 text-gray-400 bg-black hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
               <span class="absolute -inset-0.5"></span>
               <span class="sr-only">Open main menu</span>
               <!--
                 Icon when the menu is closed.
                 Menu open: "hidden", Menu closed: "block"
               -->
-              <svg v-if="!navOpen" class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <svg v-if="!navOpen" class="block h-6 w-6 left-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
               </svg>
               <!--
@@ -27,14 +27,14 @@
               </svg>
             </button>
           </div>
-          <div v-if="navOpen" class="px-8;sm:hidden p-4 rounded border">
+          <div v-if="navOpen" class="px-8;sm:hidden p-4 rounded border left-0 mt-56 ml-0 bg-black">
            <ul>
-            <li><router-link to="contact" class="text-red-800">Contact</router-link></li>
-            <li><router-link to="about" class="text-red-800">About</router-link></li>
-            <li><router-link to="prophecies" class="text-red-800">Prophecies</router-link></li>
-            <li><router-link to="sermons" class="text-red-800">Sermons</router-link></li>
-            <li><router-link to="directory" class="text-red-800">Directory</router-link></li>
-            <li><router-link to="donate" class="text-red-800">Donate</router-link></li>
+            <li><router-link to="contact" class="text-red-800 hover:text-sky-700">Contact:</router-link></li>
+            <li><router-link to="about" class="text-red-800 hover:text-sky-700">About:</router-link></li>
+            <li><router-link to="prophecies" class="text-red-800 hover:text-sky-700">Prophecies:</router-link></li>
+            <li><router-link to="sermons" class="text-red-800 hover:text-sky-700">Sermons:</router-link></li>
+            <li><router-link to="directory" class="text-red-800 hover:text-sky-700">Directory:</router-link></li>
+            <li><router-link to="donate" class="text-red-800 hover:text-sky-700">Donate:</router-link></li>
           </ul>
         </div>
 
@@ -83,7 +83,7 @@
     <!--Image Header-->
 <link href="https://fonts.googleapis.com/css?family=Lato: 100,300,400,700|Luckiest+Guy|Oxygen:300,400" rel="stylesheet">
   <link href="style.css" type="text/css" rel="stylesheet">
-    <div style="display: flex; width: 100%; justify-content: center; margin-top: 200px; margin-bottom: 50px;">
+    <div class="filter grayscale hover:filter-none transition duration-500" style="display: flex; width: 100%; justify-content: center; margin-top: 200px; margin-bottom: 50px;">
         <div style="display: grid; grid-template-columns: repeat(3, 1fr);">
         <img class="w-full h-full md:w-full md:h-md lg:w-full lg:h-full xl:w-full xl:h-full" style="border-top-left-radius: 0.375rem; border-bottom-left-radius: 0.375rem;" src="https://i.swncdn.com/media/800w/cms/CCOM/46076-old-church.800w.tn.webp">
 

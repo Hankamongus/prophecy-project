2ea9bae8faa655818e4b82a7c361ed42b188b42a
Hankamongus <hbarterburn@gmail.com>
2024-03-26 15:00:18 -0500

    another icon

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,12 @@
                                         <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
                                     </svg>
                                 </router-link></li>
-                                <li><router-link to="contact" class="text-red-800 hover:text-sky-700">Contact</router-link>▾</li>
+                                <li><router-link to="contact" class="flex items-center space-x-1 text-red-800 hover:text-sky-700">
+                                    <p>Contact</p>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-4 w-4 bi bi-envelope" viewBox="0 0 16 16">
+                                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                                    </svg>
+                                </router-link></li>
                                 <li><router-link to="about" class="text-red-800 hover:text-sky-700">About</router-link>▾</li>
                                 <li><router-link to="prophecies" class="text-red-800 hover:text-sky-700">Prophecies</router-link>▾</li>
                                 <li><router-link to="sermons" class="text-red-800 hover:text-sky-700">Sermons</router-link>▾</li>
